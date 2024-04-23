4f3e249b09e440bc73af22b3f80a0770002d1f06
starmyn <oiasmyn25@gmail.com>
2024-04-23 16:39:41 -0300

    criacao variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Iasmyn Ticiane"
+idade='16'
+email='oiasmyn25@gmail.com'
+telefone='17997463287'
+nota=10
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
