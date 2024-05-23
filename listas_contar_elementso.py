2440290fc968b5a530dd5515a97c53c01adc28c6
starmyn <oiasmyn25@gmail.com>
2024-05-23 10:40:00 -0300

    contar.elementos

---
+++ listas_contar_elementso.py
@@ -0,0 +1,5 @@
+alunos=['Adriana','Joao','Maria','Pedro','Ana',
+ 'Lucas','Mariana','Adriana']
+
+ocorrencias = alunos.count('Adriana')
+print(f"Quantidade de ocorrências de Adriana: {ocorrencias}")
