b27d634b65fcd3c578a4f813455dadd4e57d7dab
starmyn <oiasmyn25@gmail.com>
2024-05-23 10:12:12 -0300

    lista.clonar

---
+++ listas_clonar.py
@@ -0,0 +1,6 @@
+alunos=['Joao','Maria','Pedro','Ana','Lucas','Mariana']
+copiar= list(alunos)
+print(f"Lista Original: {alunos}")
+print(f"Lista Cópia: {alunos}")
+alunos.remove("Pedro")
+print(f"nome removido:{alunos}")
