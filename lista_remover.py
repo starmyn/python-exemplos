826fa2e8275f58fb06523df3422ca181ee595530
starmyn <oiasmyn25@gmail.com>
2024-05-23 09:35:51 -0300

    lista.remover

---
+++ lista_remover.py
@@ -0,0 +1,10 @@
+alunos=['Adriana','Joao','Maria','Pedro','Ana',
+ 'Lucas','Mariana','Adriana']
+print(f"Lista de alunos: {alunos}")
+alunos.remove("Adriana")
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+del alunos[3]
+print(f"Lista de alunos: {alunos}")
+alunos.clear()
+print(f"Lista de alunos: {alunos}")
